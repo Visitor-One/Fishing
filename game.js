5669e52e58c5daf837414df79f589440e7082a9b
Visitor-One <73360889+Visitor-One@users.noreply.github.com>
2026-08-28 15:57:29 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '1.0.0';
+  const VERSION = '1.1.0';
   console.log('Angelspiel V' + VERSION);
 
   const boatScene = document.getElementById('boatScene');
